598dd4b2d9137d257e787243a5a0b1b13470b81e
abusayadebu <abusayad743@gmail.com>
2025-07-28 15:36:28 +0600

    get data from local storage and shown in show.html

--- script.js
+++ script.js
@@ -3,7 +3,38 @@ function clickHandler(){
 
     const nameValue = document.getElementById("name").value;
     const emailValue = document.getElementById("email").value;
-    console.log("Entered name", nameValue);
-    console.log("entered email", emailValue);
 
+    localStorage.setItem("userName", nameValue);
+    localStorage.setItem("userEmail", emailValue);
+
+
+    // now go to the show.html file
+    window.location.href = "show.html";
+}
+
+// show.html file working here
+
+// window.onload = function(){
+//     const showName = document.getElementById("givenName");
+//     const showEmail = document.getElementById("givenEmail");
+
+//     const getName = localStorage.getItem("userName");
+//     const getEmail = localStorage.getItem("userEmail");
+
+//     showName.textContent = getName;
+//     showEmail.textContent = getEmail;
+
+// }
+
+window.onload = function(){
+    const showName = document.getElementById("givenName");
+    const showEmail = document.getElementById("givenEmail");
+
+    if(showName && showEmail) {  // <-- Check if elements exist
+        const getName = localStorage.getItem("userName");
+        const getEmail = localStorage.getItem("userEmail");
+
+        showName.textContent = getName;
+        showEmail.textContent = getEmail;
+    }
 }
