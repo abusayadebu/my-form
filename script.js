73b5a57bed40b666068af031025725361e8800fe
abusayadebu <abusayad743@gmail.com>
2025-07-28 14:05:35 +0600

    button click handler addedusing onclick and input value get by id

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("beta ki khobor")
+function clickHandler(){
+    event.preventDefault();
+
+    const nameValue = document.getElementById("name").value;
+    const emailValue = document.getElementById("email").value;
+    console.log("Entered name", nameValue);
+    console.log("entered email", emailValue);
+
+}
